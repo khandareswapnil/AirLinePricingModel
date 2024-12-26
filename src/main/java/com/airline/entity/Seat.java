702417186b3f86bb725d8ff228a791a287153040
Shivaji Shendge <shivajishendge2001@gmail.com>
2024-12-26 14:44:37 +0530

    Booking Added

---
+++ src/main/java/com/airline/entity/Seat.java
@@ -0,0 +1,11 @@
+package com.airline.entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Seat {
+	private int sno;
+	private int status;
+}
--- src/main/java/com/airline/user/service/BookingService.java
+++ src/main/java/com/airline/user/service/BookingService.java
@@ -2,13 +2,18 @@ package com.airline.user.service;
 
 import java.util.List;
 
+import com.airline.entity.Seat;
 import com.airline.entity.ViewFlightsScheduleByUser;
 import com.airline.user.repo.BookingRepo;
 
 public class BookingService {
 	BookingRepo bookrepo= new BookingRepo();
-	public List<Integer> showAvailableSeats (int fsid) {
+	public List<Seat> showAvailableSeats (int fsid) {
 		return bookrepo.showAvailableSeats(fsid);
 	}
-
+    
+	public boolean bookTicket(int uid, int fsid, int seatNo) {
+		
+		return bookrepo.bookTicket(uid, fsid, seatNo);	
+	}
 }
